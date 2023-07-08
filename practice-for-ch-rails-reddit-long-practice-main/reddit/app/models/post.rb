4bec5b57bc5eee53a85f358d704a75b8bc364773
Elliot Chang <elliotchang126@gmail.com>
2023-07-07 17:08:01 -0700

    Completed post migration

--- practice-for-ch-rails-reddit-long-practice-main/reddit/app/models/post.rb
+++ practice-for-ch-rails-reddit-long-practice-main/reddit/app/models/post.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  title      :string           not null
+#  url        :string
+#  content    :text
+#  sub_id     :bigint           not null
+#  author_id  :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Post < ApplicationRecord
+    validates :title, :sub_id, :author_id, presence: true
 end
